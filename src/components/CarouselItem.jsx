a05d489e4b521ee05e5fc5b94386e02bd93da1e5
CJengineer18 <cjjd18.e2@gmail.com>
2020-10-21 20:54:50 -0500

    Day 2: The Player

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { setFavorite, deleteFavorite } from "../actions";
-import "../assets/styles/components/CarouselItem.scss";
-import playIcon from "../assets/static/play-icon.png";
-import plusIcon from "../assets/static/plus-icon.png";
-import removeIcon from "../assets/static/remove-icon.png";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setFavorite, deleteFavorite } from '../actions';
+import '../assets/styles/components/CarouselItem.scss';
+import playIcon from '../assets/static/play-icon.png';
+import plusIcon from '../assets/static/plus-icon.png';
+import removeIcon from '../assets/static/remove-icon.png';
 
 const CarouselItem = (props) => {
   const { id, cover, title, year, contentRating, duration, isList } = props;
@@ -23,11 +24,15 @@ const CarouselItem = (props) => {
       <img className='carousel-item__img' src={cover} alt={title} />
       <div className='carousel-item__details'>
         <div>
-          <img
-            className='carousel-item__details--img'
-            src={playIcon}
-            alt='Play Icon'
-          />
+          <div>
+            <Link to={`/player/${id}`}>
+              <img
+                className='carousel-item__details--img'
+                src={playIcon}
+                alt='Play Icon'
+              />
+            </Link>
+          </div>
           {isList ? (
             <img
               className='carousel-item__details--img'
